8a86fe2c3b1410a3e9d83b0fbe7c0eb85b480567
Matheus Amaral da Costa <161625293+MatheusADC@users.noreply.github.com>
2025-05-26 13:13:08 -0300

    add: Esqueleto do projeto

---
+++ storage/framework/views/c1b5255df93db07e4aba633b5e6dd18c.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="<?php echo e(str_replace('_', '-', app()->getLocale())); ?>">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Celke</title>
+    </head>
+    <body>
+        <h1>Bem-vindo à Celke</h1>
+    </body>
+</html>
+<?php /**PATH C:\xampp\htdocs\celke\resources\views/welcome.blade.php ENDPATH**/ ?>
