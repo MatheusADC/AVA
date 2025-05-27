63339225d50a69ef979bf85853b3dd417f4af365
Matheus Amaral da Costa <161625293+MatheusADC@users.noreply.github.com>
2025-05-27 16:32:16 -0300

    add: CoursesController

---
+++ app/Http/Controllers/CoursesController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CoursesController extends Controller
+{
+    // Listar os cursos
+    public function index()
+    {
+        dd("Carregar os cursos");
+    }
+
+}
