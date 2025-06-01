a5880273336e4a48fdbce15b041681478bcbfa17
Matheus Amaral da Costa <161625293+MatheusADC@users.noreply.github.com>
2025-05-31 22:12:11 -0300

    add: Métodos create e store em CoursesController

--- app/Http/Controllers/CoursesController.php
+++ app/Http/Controllers/CoursesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Course;
 use Illuminate\Http\Request;
 
 class CoursesController extends Controller
@@ -13,4 +14,23 @@ class CoursesController extends Controller
         return view('courses.index');
     }
 
+    // Carregar o formulário cadastrar novo curso
+    public function create()
+    {
+        // Carregar a view
+        return view('courses.create');
+    }
+
+    //Cadastrar no banco de dados o novo curso
+    public function store(Request $request)
+    {
+        // dd($request);
+        // Cadastrar no banco de dados na tabela cursos
+        Course::create([
+            'name' => $request->name
+        ]);
+
+        // Redirecionar o usuário, enviar a mensagem de sucesso
+        return redirect()->route('courses.index')->with('success', 'Curso cadastrado com sucesso!');
+    }
 }
