aa303b014f9582bd976f8aaa33b06e5ef0c6dee2
Matheus Amaral da Costa <161625293+MatheusADC@users.noreply.github.com>
2025-06-04 12:52:47 -0300

    add: View Layout

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Celke</title>
+</head>
+
+<body>
+    <a href="{{ route('courses.index') }}">Cursos</a><br>
+    <a href="{{ route('courses-status.index') }}">Status Cursos</a><br>
+    <a href="{{ route('courses-batches.index') }}">Turmas</a><br>
+    <a href="{{ route('modules.index') }}">Módulos</a><br>
+    <a href="{{ route('lessons.index') }}">Aulas</a><br>
+    <a href="{{ route('users.index') }}">Usuários</a><br>
+    <a href="{{ route('status.index') }}">Status</a><br>
+
+    @yield('content')
+    
+</body>
+
+</html>
