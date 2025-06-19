8fa0f311cad3730da0351d7ec21a611088882b62
Matheus Amaral da Costa <161625293+MatheusADC@users.noreply.github.com>
2025-06-19 12:24:04 -0300

    fix: Rotas em create do cursos status

--- resources/views/courses_status/create.blade.php
+++ resources/views/courses_status/create.blade.php
@@ -1,15 +1,17 @@
-<div>
-    <h2>Cadastrar Status Cursos</h2>
+@extends('layouts.admin')
 
-    <a href="{{ route('courses-status.index') }}">Listar</a><br><br>
+@section('content')
+    <h2>Cadastrar o Statuses dos Cursos</h2>
 
-    <form action="{{ route('courses-status.store') }}" method="POST">
+    <a href="{{ route('course_statuses.index') }}">Listar</a><br><br>
+
+    <form action="{{ route('course_statuses.store') }}" method="POST">
+        {{-- Só aceita valores da própria aplicação, não recebendo de forma externa, aumentando a segurança --}}
         @csrf
         @method('POST')
 
         <label>Nome: </label>
         <input type="text" name="name" id="name" placeholder="Nome do status" required><br><br>
-
         <button type="submit">Cadastrar</button>
     </form>
-</div>
+@endsection
