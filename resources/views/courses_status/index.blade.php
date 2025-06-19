350f4b1707004aec869e83115a444afa67820e60
Matheus Amaral da Costa <161625293+MatheusADC@users.noreply.github.com>
2025-06-19 12:24:29 -0300

    fix: Rotas em index do cursos status

--- resources/views/courses_status/index.blade.php
+++ resources/views/courses_status/index.blade.php
@@ -1,8 +1,18 @@
-<div>
+@extends('layouts.admin')
+
+@section('content')
     <h2>Listar os Status Cursos</h2>
+
+    <a href="{{ route('course_statuses.create') }}">Cadastrar</a><br><br>
 
     <x-alert />
 
-    <a href="{{ route('courses-status.create') }}">Cadastrar</a>
-    
-</div>
+    {{-- Imprimir o registro --}}
+    @forelse ($coursesStatuses as $courseStatus)
+        ID: {{ $courseStatus->id }}<br>
+        Nome: {{ $courseStatus->name }}<br>
+        <a href="{{ route('course_statuses.show', ['courseStatus' => $courseStatus->id]) }}">Visualizar</a>
+    @empty
+        Nenhum registro encontrado!
+    @endforelse
+@endsection
