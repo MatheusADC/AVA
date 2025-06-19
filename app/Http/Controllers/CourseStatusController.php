6a4f35a784ffc7163777a475574f9ef0346bc262
Matheus Amaral da Costa <161625293+MatheusADC@users.noreply.github.com>
2025-06-19 12:22:35 -0300

    add: Função show para o cursos status

--- app/Http/Controllers/CourseStatusController.php
+++ app/Http/Controllers/CourseStatusController.php
@@ -10,14 +10,24 @@ class CourseStatusController extends Controller
     // Listar os status cursos
     public function index()
     {
-        // Carregar a view 
-        return view('courses_status.index');
+        // Recuperar os registros do banco de dados
+        $coursesStatuses = CourseStatus::orderBy('id','DESC')->get();
+
+        // Carregar a view
+        return view('courses_status.index', ['coursesStatuses' => $coursesStatuses]);
+    }
+
+    // Visualizar os detalhes do status para curso
+    public function show(CourseStatus $courseStatus)
+    {
+        // Carregar a view
+        return view('courses_status.show', ['courseStatus' => $courseStatus]);
     }
 
     // Carregar o formulário cadastrar novo status curso
     public function create()
     {
-        // Carregar a view 
+        // Carregar a view
         return view('courses_status.create');
     }
 
@@ -31,6 +41,6 @@ class CourseStatusController extends Controller
         ]);
 
         // Redirecionar o usuário, enviar a mensagem de sucesso
-        return redirect()->route('courses-status.index')->with('success', 'Status cadastrado com sucesso!');
+        return redirect()->route('course_statuses.index')->with('success', 'Status cadastrado com sucesso!');
     }
 }
