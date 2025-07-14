7d43f43916cdc53715eb61a26e37f033ce6708ee
Matheus Amaral da Costa <161625293+MatheusADC@users.noreply.github.com>
2025-07-14 19:54:49 -0300

    add: Link para Editar Curso

--- resources/views/courses/index.blade.php
+++ resources/views/courses/index.blade.php
@@ -13,7 +13,8 @@
     @forelse ($courses as $course)
         ID: {{ $course->id }}<br>
         Nome: {{ $course->name }}<br>
-        <a href={{ route('courses.show', ['course' => $course->id]) }}>Visualizar</a>
+        <a href={{ route('courses.show', ['course' => $course->id]) }}>Visualizar</a><br>
+        <a href={{ route('courses.edit', ['course' => $course->id]) }}>Editar</a><br>
         <hr>
     @empty
 
