edb935f981c74c7b853d6c434fe26b326b66e2e5
Matheus Amaral da Costa <161625293+MatheusADC@users.noreply.github.com>
2025-07-14 19:52:37 -0300

    add: Rotas edit & update para cursos

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,8 @@ Route::prefix('courses')->group(function () {
     Route::get('/create', [CourseController::class, 'create'])->name('courses.create');
     Route::get('/{course}', [CourseController::class, 'show'])->name('courses.show');
     Route::post('/', [CourseController::class, 'store'])->name('courses.store');
+    Route::get('/{course}/edit', [CourseController::class, 'edit'])->name('courses.edit');
+    Route::put('/{course}', [CourseController::class, 'update'])->name('courses.update');
 });
 
 // Cursos Status
