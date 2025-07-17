f3725741ae50f591a0d3f6399e02c004e844d7b4
Matheus Amaral da Costa <161625293+MatheusADC@users.noreply.github.com>
2025-07-16 21:13:27 -0300

    add: Botão para apagar curso

--- resources/views/courses/index.blade.php
+++ resources/views/courses/index.blade.php
@@ -15,6 +15,13 @@
         Nome: {{ $course->name }}<br>
         <a href={{ route('courses.show', ['course' => $course->id]) }}>Visualizar</a><br>
         <a href={{ route('courses.edit', ['course' => $course->id]) }}>Editar</a><br>
+        <form action="{{ route('courses.destroy', ['course' => $course->id]) }}" method="POST">
+            @csrf
+            @method('delete')
+
+            <button type="submit" onclick="return confirm('Tem certeza que deseja apagar este registro?')">Apagar</button>
+
+        </form>
         <hr>
     @empty
 
