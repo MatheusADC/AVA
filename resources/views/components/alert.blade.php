6dc85187db396ee76c240deb95d115e7395dbf20
Matheus Amaral da Costa <161625293+MatheusADC@users.noreply.github.com>
2025-07-23 01:05:06 -0300

    add: Erros da Validação

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -9,3 +9,12 @@
         {{ session('error') }}
     </p>
 @endif
+
+{{-- Erros da Validação --}}
+@if ($errors->any())
+    <p style="color: #f00">
+        @foreach ($errors->all() as $error)
+            {{ $error }}<br>
+        @endforeach
+    </p>
+@endif
