42f5617c8fd176dd62c27f305f6aa26c9a587ed5
Matheus Amaral da Costa <161625293+MatheusADC@users.noreply.github.com>
2025-08-29 18:22:46 -0300

    feat: Página de Visualização em courses_batches

---
+++ resources/views/courses_batches/show.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.admin')
+
+@section('content')
+    <h2>Detalhes da Turma</h2>
+
+    <a href="{{ route('course_batches.index') }}">Listar</a><br>
+    <a href="{{ route('course_batches.edit', ['courseBatch' => $courseBatch->id]) }}">Editar</a><br>
+
+    <form action="{{ route('course_batches.destroy', ['courseBatch' => $courseBatch->id]) }}" method="POST">
+        @csrf
+        @method('delete')
+
+        <button type="submit" onclick="return confirm('Tem certeza que deseja apagar este registro?')">Apagar</button>
+
+    </form><br><br>
+
+    <x-alert />
+
+    {{-- Imprimir o registro --}}
+    ID: {{ $courseBatch->id }}<br>
+    Nome: {{ $courseBatch->name }}<br>
+    Curso: {{ $courseBatch->course->name }}<br>
+    Cadastrado: {{ \Carbon\Carbon::parse($courseBatch->created_at)->format('d/m/Y H:i:s') }}<br>
+    Editado: {{ \Carbon\Carbon::parse($courseBatch->updated_at)->format('d/m/Y H:i:s') }}<br>
+@endsection
